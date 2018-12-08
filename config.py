72ef97efdd0691f6bc5eb91b009199984a1ebde5
lyabakhp <lyabakhp@gmail.com>
2018-12-08 19:58:49 +0200

    Database, Log in, Session

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'my-secret-key'
-    SQLALHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALHEMY_TRACK_MODIFICAIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- microblog.py
+++ microblog.py
@@ -1,1 +1,5 @@
 from app import app
+from app.models import db, User, Post
+@app.shell_context_processor
+def maka_shell_context():
+    return {'db':db, 'User':User, 'Post':Post}
