60077b757b28796fee2b6a8f41408818f7078b25
lyabakhP <lyabakhp@gmail.com>
2018-12-10 22:00:14 +0200

    Changed structure

--- app/errors.py
+++ app/errors.py
@@ -1,12 +1,12 @@
-from flask import render_template
-from app import app, db
+# from flask import render_template
+# from app import app, db
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
+# @app.errorhandler(404)
+# def not_found_error(error):
+#     return render_template('404.html'), 404
 
-@app.errorhandler(500)
-def internal_error(error):
-    # if some ckanges interrupted by error
-    db.session.rollback()
-    return render_template('500.html'), 500
+# @app.errorhandler(500)
+# def internal_error(error):
+#     # if some ckanges interrupted by error
+#     db.session.rollback()
+#     return render_template('500.html'), 500
--- microblog.py
+++ microblog.py
@@ -1,5 +1,16 @@
-from app import app
-from app.models import db, User, Post
+# from app import app
+# from app.models import db, User, Post
+# @app.shell_context_processor
+# def maka_shell_context():
+#     return {'db':db, 'User':User, 'Post':Post}
+
+from app import create_app, db, cli
+from app.models import User, Post
+
+app = create_app()
+cli.register(app)
+
+
 @app.shell_context_processor
-def maka_shell_context():
-    return {'db':db, 'User':User, 'Post':Post}
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
